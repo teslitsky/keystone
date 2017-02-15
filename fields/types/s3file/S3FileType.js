470ec851e5aabb6331e0061339f77d56cf7baf6d
Dmitry Teslitsky <dmitryteslitsky@gmail.com>
2017-02-15 21:46:52 +0200

    Fix Object.assign

--- fields/types/s3file/S3FileType.js
+++ fields/types/s3file/S3FileType.js
@@ -9,6 +9,7 @@ See https://github.com/keystonejs/keystone/wiki/File-Fields-Upgrade-Guide
 /* eslint-disable */
 
 var loggedWarning = false;
+var assign = Object.assign;
 
 /**
  * S3File FieldType Constructor
@@ -73,16 +74,16 @@ s3file.prototype.addToSchema = function (schema) {
 
 	var paths = this.paths = {
 		// fields
-		filename: this.path + '.filename',
-		originalname: this.path + '.originalname',
-		path: this.path + '.path',
-		size: this.path + '.size',
-		filetype: this.path + '.filetype',
-		url: this.path + '.url',
+		filename: this._path.append('.filename'),
+		originalname: this._path.append('.originalname'),
+		path: this._path.append('.path'),
+		size: this._path.append('.size'),
+		filetype: this._path.append('.filetype'),
+		url: this._path.append('.url'),
 		// virtuals
-		exists: this.path + '.exists',
-		upload: this.path + '_upload',
-		action: this.path + '_action',
+		exists: this._path.append('.exists'),
+		upload: this._path.append('_upload'),
+		action: this._path.append('_action'),
 	};
 
 	var schemaPaths = this._path.addTo({}, {
@@ -321,7 +322,7 @@ s3file.prototype.generateHeaders = function (item, file, callback) {
 	}
 
 	if (validateHeaders(customHeaders, callback)) {
-		headers = assign(headers, customHeaders);
+		headers = Object.assign(headers, customHeaders);
 	}
 
 	return headers;
